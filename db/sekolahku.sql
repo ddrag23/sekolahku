d9a07d8d19ab2a3b98a956bc76cf8fdc9d1e1a93
ddrag23 <kurodyami@gmail.com>
2020-08-19 09:50:35 +0700

    add db

--- db/sekolahku.sql
+++ db/sekolahku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2020 at 11:07 AM
+-- Generation Time: Aug 19, 2020 at 02:49 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.30
 
@@ -45,8 +45,8 @@ CREATE TABLE `gelombang` (
 --
 
 INSERT INTO `gelombang` (`id_gelombang`, `sesi_gelombang`, `awal`, `akhir`, `biaya`, `tahun_ajaran`, `created_by`, `date_created`, `modified_by`, `modified_date`) VALUES
-(1, 'gelombang 1', '12-06-2020', '12-07-2020', '100000', '2020/2021', 37, '2020-06-29 14:34:13', 37, '2020-06-29 14:34:13'),
-(7, 'gelombang 2', '12-07-2020', '12-08-2020', '150000', '2020 / 2021', 37, '2020-06-29 10:21:31', NULL, '2020-06-29 17:21:31');
+(11, 'gelombang 1', '12-07-2020', '12-08-2020', '989987', '2020 / 2021', 37, '2020-08-01 06:36:51', NULL, '2020-08-01 13:36:51'),
+(12, 'gelombang 2', '12-07-2020', '12-08-2020', '23123', '2020 / 2021', 37, '2020-08-01 07:29:11', NULL, '2020-08-01 14:29:11');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,9 @@ INSERT INTO `guru` (`id_guru`, `nip`, `nama_guru`, `alamat_guru`, `gender_guru`,
 (6, 98239813, 'sopyan', 'sidoarjo', 'laki-laki', 929292, 37, '2020-06-28 02:54:24', 0, '0000-00-00 00:00:00'),
 (7, 23131231, 'paijo', 'gresik', 'laki-laki', 82828, 37, '2020-06-28 02:54:42', 0, '0000-00-00 00:00:00'),
 (8, 4323411, 'lilis', 'porong', 'perempuan', 88888, 37, '2020-06-28 02:55:02', 0, '0000-00-00 00:00:00'),
-(9, 12313131, 'didik', 'keboguyang', 'laki-laki', 929292, 37, '2020-06-28 05:36:32', 0, '0000-00-00 00:00:00');
+(9, 12313131, 'didik', 'keboguyang', 'laki-laki', 929292, 37, '2020-06-28 05:36:32', 0, '0000-00-00 00:00:00'),
+(10, 2147483647, 'paijo wae', 'akjasjkdna', 'laki-laki', 929292, 37, '2020-08-01 06:28:24', 0, '0000-00-00 00:00:00'),
+(11, 98239813, 'paijo jos', 'sidoarjo', 'laki-laki', 929292, 37, '2020-08-01 07:30:59', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -102,7 +104,9 @@ INSERT INTO `kelas` (`id_kelas`, `guru_id`, `nama_kelas`, `created_by`, `date_cr
 (7, 7, '1B', 37, '2020-06-28 02:55:24', NULL, '0000-00-00 00:00:00'),
 (8, 8, '2A', 37, '2020-06-28 02:55:30', NULL, '0000-00-00 00:00:00'),
 (9, 9, '1C', 37, '2020-06-28 05:36:51', NULL, '0000-00-00 00:00:00'),
-(10, 9, '3A', 37, '2020-06-30 05:17:50', NULL, '0000-00-00 00:00:00');
+(10, 9, '3A', 37, '2020-06-30 05:17:50', NULL, '0000-00-00 00:00:00'),
+(11, 10, '4A', 37, '2020-08-01 06:28:39', NULL, '0000-00-00 00:00:00'),
+(13, 11, '1D', 51, '2020-08-01 07:51:24', NULL, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -128,7 +132,12 @@ CREATE TABLE `nilai` (
 INSERT INTO `nilai` (`id_nilai`, `ppdb_id`, `jum_nilai`, `status_ppdb`, `date_created`, `created_by`, `modified_by`, `modified_date`) VALUES
 (10, 29, 40, 'tidak lulus', '2020-06-28 06:11:57', 37, 37, '2020-07-01 14:13:46'),
 (11, 35, 30, 'tidak lulus', '2020-06-30 05:14:42', 37, 37, '2020-07-04 01:52:53'),
-(18, 35, 92, 'lulus', '2020-07-01 14:13:36', 37, 0, '0000-00-00 00:00:00');
+(18, 35, 92, 'lulus', '2020-07-01 14:13:36', 37, 0, '0000-00-00 00:00:00'),
+(19, 35, 10, 'tidak lulus', '2020-07-05 11:14:43', 37, 0, '0000-00-00 00:00:00'),
+(20, 35, 90, 'lulus', '2020-07-24 08:00:11', 37, 0, '0000-00-00 00:00:00'),
+(21, 49, 90, 'lulus', '2020-08-01 07:50:26', 51, 0, '0000-00-00 00:00:00'),
+(22, 50, 90, 'lulus', '2020-08-01 08:19:19', 51, 0, '0000-00-00 00:00:00'),
+(23, 51, 90, 'lulus', '2020-08-04 17:32:53', 37, 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -151,7 +160,7 @@ CREATE TABLE `pengumuman` (
 --
 
 INSERT INTO `pengumuman` (`id_pengumuman`, `file_pengumuman`, `status_pengumuman`, `created_by`, `date_created`, `modified_by`, `modified_created`) VALUES
-(6, 'seleksi20200630.pdf', 'publis', 37, '2020-06-30 10:31:18', 0, '2020-06-30 17:31:18');
+(7, 'seleksi20200801.pdf', 'publis', 37, '2020-08-01 07:29:51', 0, '2020-08-01 14:29:51');
 
 -- --------------------------------------------------------
 
@@ -186,7 +195,15 @@ CREATE TABLE `ppdb` (
 
 INSERT INTO `ppdb` (`id_ppdb`, `user_id`, `nama_ppdb`, `nama_panggilan`, `gender_ppdb`, `tempat_lahir_ppdb`, `tanggal_lahir_ppdb`, `asal_sekolah_ppdb`, `alamat_sekolah_ppdb`, `alamat_rumah_ppdb`, `no_hp_ppdb`, `nama_ortu_ppdb`, `sesi_gelombang`, `status_pembayaran`, `created_by`, `date_created`, `modified_by`, `date_modified`) VALUES
 (29, 56, 'user baru', 'user', 'laki-laki', 'sidoarjo', '1997-02-28', 'RA tadika mesra', 'pepe', 'pepe tani sawah sedati sidoarjo', 2147483647, 'lali', '', 'lunas', 56, '2020-06-28 06:11:20', 0, '0000-00-00 00:00:00'),
-(35, 66, 'riki andri hermanto', 'andri', 'laki-laki', 'sidoarjo', '2004-06-02', 'RA Hasyim asy\'ari', 'pepe sedati', 'jl.pepe tani sawah no 3, dusun pepe tani sawah, desa pepe sedati sidoarjo', 2147483647, 'fiki', 'gelombang 1', 'lunas', 66, '2020-06-30 05:10:06', 37, '2020-06-30 05:13:36');
+(35, 66, 'riki andri hermanto', 'andri', 'laki-laki', 'sidoarjo', '2004-06-02', 'RA Hasyim asy\'ari', 'pepe sedati', 'jl.pepe tani sawah no 3, dusun pepe tani sawah, desa pepe sedati sidoarjo', 2147483647, 'fiki', 'gelombang 1', 'lunas', 66, '2020-06-30 05:10:06', 37, '2020-06-30 05:13:36'),
+(36, 67, 'coba kali', 'keles', 'laki-laki', 'mariki', '2020-07-23', 'ra kepo', 'wes lali bos', 'sidoarjo', 2147483647, 'shirogane yuga', 'gelombang 1', 'lunas', 67, '2020-07-06 07:12:23', 37, '2020-07-15 12:16:16'),
+(37, 70, 'coba', 'arch', 'laki-laki', 'kono', '2020-02-22', 'sadkjha', 'kok', 'kajhkja', 2147483647, 'pp', 'gelombang 2', 'lunas', 37, '2020-08-01 06:27:37', 0, '0000-00-00 00:00:00'),
+(38, 71, 'coba', 'arch', 'laki-laki', 'kono', '2019-12-31', 'asdas', 'asdkjab', 'jaskdj', 879879, 'kasdn', 'gelombang 2', 'lunas', 37, '2020-08-01 06:31:35', 0, '0000-00-00 00:00:00'),
+(39, 72, 'test1', 'arch', 'laki-laki', 'kono', '0000-00-00', 'ra kepo', 'kajsndk', 'asdas', 87876, 'kjkh', 'gelombang 2', 'lunas', 37, '2020-08-01 06:35:14', 0, '0000-00-00 00:00:00'),
+(40, 73, 'coba', 'arch', 'laki-laki', 'kono', '2019-12-30', 'sadkjha', 'kjsandkja', 'asdas', 987979, 'asbdjb', 'gelombang 1', 'lunas', 37, '2020-08-01 06:37:31', 51, '2020-08-01 07:49:30'),
+(49, 77, 'farel', 'farel', 'laki-laki', 'kono', '2020-08-01', 'ra sidoarjo', 'pepe', 'sidoarjo', 2147483647, 'lali', 'gelombang 1', 'lunas', 51, '2020-08-01 07:48:59', 0, '0000-00-00 00:00:00'),
+(50, 78, 'dinda', 'dinda', 'perempuan', 'sidoarjo', '2020-08-01', 'ra sidoarjo', 'pepe', 'sidoarjo', 2147483647, 'kkkkk', 'gelombang 1', 'lunas', 78, '2020-08-01 08:18:23', 51, '2020-08-01 08:19:03'),
+(51, 79, 'sopo jarwo', 'sopo', 'laki-laki', 'sda', '2020-08-05', 'ra kepo', 'pp', 'sidoarjo', 2147483647, 'lali', 'gelombang 1', NULL, 79, '2020-08-04 17:31:11', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -294,7 +311,12 @@ INSERT INTO `siswa` (`id_siswa`, `users_id`, `kelas_id`, `foto`, `nis`, `nisn`, 
 (38, NULL, 6, 'default.png', '21312312312', '87987999797', '123123121', '232131321', 'anas', 'pepe tani sedati sidoarj', 'kalibogo', 8, 8, 'pepe', 'sedati', 'sidoarjo', 'jawa timu', 8263, 'sidorarjo', '21-04-2009', 'islam', 'aktif', 8, 38, 40, 'o', 'tidak ada', 'laki-laki', 2, 'makan', 'chef', 'RA', 'hasyim asyar', 'jalan kaki', 'tidak yatim/piatu', 'sodik', 'rini', 63886836, 76883686, 'D1', 'SD sederajat', 'PNS', 'petani', 'lebih dari 3 juta', '', '980183098', 'orang tua', '1jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020 / 2021', 0, '2020-06-30 15:43:35', 0, '2020-06-30 15:43:35'),
 (39, NULL, 6, 'default.png', '222222222', '878798797', '213213123', '21312314555', 'tatik', 'pepe tani sedati sidoarj', 'kalibogo', 8, 8, 'pepe', 'sedati', 'sidoarjo', 'jawa timu', 8263, 'sidorarjo', '21-04-2009', 'islam', 'aktif', 8, 38, 40, 'o', 'tidak ada', 'laki-laki', 2, 'makan', 'chef', 'RA', 'hasyim asyar', 'jalan kaki', 'tidak yatim/piatu', 'sodik', 'rini', 63886836, 76883686, 'D1', 'SD sederajat', 'PNS', 'petani', 'lebih dari 3 juta', '', '980183098', 'orang tua', '1jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020 / 2021', 0, '2020-06-30 15:43:35', 0, '2020-06-30 15:43:35'),
 (40, NULL, 6, 'default.png', '321231312', '97987997977', '232112222', '3211214444', 'lilik', 'pepe tani sedati sidoarj', 'kalibogo', 8, 8, 'pepe', 'sedati', 'sidoarjo', 'jawa timu', 8263, 'sidorarjo', '21-04-2009', 'islam', 'aktif', 8, 38, 40, 'o', 'tidak ada', 'laki-laki', 2, 'makan', 'chef', 'RA', 'hasyim asyar', 'jalan kaki', 'tidak yatim/piatu', 'sodik', 'rini', 63886836, 76883686, 'D1', 'SD sederajat', 'PNS', 'petani', 'lebih dari 3 juta', '', '980183098', 'orang tua', '1jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020 / 2021', 0, '2020-06-30 15:43:35', 0, '2020-06-30 15:43:35'),
-(41, NULL, 6, 'default.png', '233333222', '7978978989', '3333333111', '553453444', 'nur', 'pepe tani sedati sidoarj', 'kalibogo', 8, 8, 'pepe', 'sedati', 'sidoarjo', 'jawa timu', 8263, 'sidorarjo', '21-04-2009', 'islam', 'aktif', 8, 38, 40, 'o', 'tidak ada', 'laki-laki', 2, 'makan', 'chef', 'RA', 'hasyim asyar', 'jalan kaki', 'tidak yatim/piatu', 'sodik', 'rini', 63886836, 76883686, 'D1', 'SD sederajat', 'PNS', 'petani', 'lebih dari 3 juta', '', '980183098', 'orang tua', '1jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020 / 2021', 0, '2020-06-30 15:43:35', 0, '2020-06-30 15:43:35');
+(41, NULL, 6, 'default.png', '233333222', '7978978989', '3333333111', '553453444', 'nur', 'pepe tani sedati sidoarj', 'kalibogo', 8, 8, 'pepe', 'sedati', 'sidoarjo', 'jawa timu', 8263, 'sidorarjo', '21-04-2009', 'islam', 'aktif', 8, 38, 40, 'o', 'tidak ada', 'laki-laki', 2, 'makan', 'chef', 'RA', 'hasyim asyar', 'jalan kaki', 'tidak yatim/piatu', 'sodik', 'rini', 63886836, 76883686, 'D1', 'SD sederajat', 'PNS', 'petani', 'lebih dari 3 juta', '', '980183098', 'orang tua', '1jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020 / 2021', 0, '2020-06-30 15:43:35', 0, '2020-06-30 15:43:35'),
+(42, NULL, 6, 'item-20200726.png', '222222255522', '123456733', '12312312', '9839873', 'user baru guys', 'suroboyo ', 'pepe tani', 12, 6, 'pepe', 'sedati', 'sidoarjo', 'jawa timur', 61253, 'sidoarjo', '12-12-1997', 'islam', 'aktif', 8, 8, 8, 'o', 'gk onok guys', 'laki-laki', 2, 'dolen', 'orep enak bos', 'TK', 'tadika mesra', 'getek', 'tidak yatim/piatu', 'kok', 'kok', 876876, 876876, 'S2', 'tidak sekolah', 'lainnya', 'tidak bekerja', 'kurang dari 1 juta', '1 juta sampai 2 juta', '098989', 'orang tua', '1 jam', '0 sampai 1km', 'sungai', 'sumber', 'PDAM', 'tembok', 'keramik', 'lampu listrik', 'sadkjn', 'SD sederajat', 'buruh', '1 juta sampai 2 juta', '0898989889', NULL, NULL, NULL, '2020 / 2021', 37, '2020-07-26 05:03:20', 51, '2020-08-01 07:54:32'),
+(43, NULL, 10, 'item_20200726.png', '11111111213', '123456723213', '983274981111', '983987311', 'user barurr', 'adsadada  ', 'pepe tani', 8, 6, 'pepe', 'sedati', 'sidoarjo', 'jawa timur', 61253, 'sidoarjo', '12-12-2002', 'islam', 'alumni', 8, 8, 8, 'o', '', 'laki-laki', 2, 'dolen', 'orep enak bos', 'TK', 'tadika mesra', 'kendaraan pribadi', 'tidak yatim/piatu', 'kok', 'kok', 876876, 876876, 'putus SD', 'tidak sekolah', 'nelayan', 'tidak bekerja', 'kurang dari 1 juta', '1 juta sampai 2 juta', '0998938', 'saudara', '1 jam', 'lebih dari 3km', 'sumber', 'sumber', 'sumber', 'papan', 'tegel', 'lampu minyak', 'sadkjn', 'tidak sekolah', 'peternak', '1 juta sampai 2 juta', '0898989889', NULL, '20-04-2020', 'sudah diambil', '2020 / 2021', 37, '2020-07-26 05:20:48', 37, '2020-08-01 06:25:55'),
+(44, NULL, 9, 'item_20200801.png', '1111111132', '123456733223', '983274982211', '983987321', 'user baru 2', 'sidoarjo  ', 'pepe tani', 12, 6, 'pepe', 'sedati', 'sidoarjo', 'jawa timur', 61253, 'sidoarjo', '12-12-2002', 'islam', 'mutasi', 8, 8, 8, 'o', '', 'laki-laki', 2, 'dolen', 'orep enak bos', 'RA', 'tadika mesra', 'jalan kaki', 'tidak yatim/piatu', 'kok', 'kok', 876876, 876876, 'S2', 'S3', 'pensiunan', 'pensiunan', 'lebih dari 3 juta', '1 juta sampai 2 juta', '098989', 'orang tua', '1 jam', 'lebih dari 3km', 'sumber', 'PDAM', 'sungai', 'klenengan', 'keramik', 'lampu listrik', '', '', '', '', '', 'https://docs.google.com/spreadsheets/d/1bnJmyv03p1cTdOS1lHg2qsnHwm2ya1HWwClQ6GIwuYQ/edit?usp=sharing', NULL, NULL, '2020 / 2021', 37, '2020-08-01 06:22:02', 37, '2020-08-01 06:24:52'),
+(45, NULL, 6, 'item_202008011.png', '1111112313', '12345674213', '98327498211', '983987332', 'user baru 3', 'sidoarjo    ', 'pepe tani', 8, 6, 'pepe', 'sedati', 'sidoarjo', 'jawa timur', 61253, 'sidoarjo', '12-12-2002', 'islam', 'alumni', 8, 8, 8, 'o', 'tidak', 'laki-laki', 2, 'renang', 'renang', 'RA', 'tadika mesra', 'jalan kaki', 'tidak yatim/piatu', 'kok', 'kok', 876876, 876876, 'SD sederajat', 'putus SD', 'tidak bekerja', 'nelayan', 'lebih dari 3 juta', 'Kurang dari 1 juta', '098989', 'orang tua', '1 jam', '0 sampai 1km', 'sumber', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', '', '', '', '', '', NULL, '20-04-2020', 'sudah diambil', '2020 / 2021', 37, '2020-08-01 07:26:47', 37, '2020-08-01 07:28:32'),
+(47, 78, NULL, 'item_202008013.png', NULL, NULL, '9832749832', '9839873321', 'dinda', 'sidoarjo', 'pepe tani', 12, 6, 'pepe', 'sedati', 'sidoarjo', 'jawa timur', 61253, 'sidoarjo', '12-12-2002', 'islam', 'aktif', 8, 8, 8, 'o', '', 'perempuan', 2, 'renang', 'renang', 'TK', 'tadika mesra', 'jalan kaki', 'tidak yatim/piatu', 'kok', 'kok', 876876, 876876, 'putus SD', 'putus SD', 'nelayan', 'petani', '1 sampai 2 juta', 'Kurang dari 1 juta', '098989', 'orang tua', '1 jam', '1km', 'kamar mandi', 'PDAM', 'PDAM', 'tembok', 'keramik', 'lampu listrik', 'sadkjn', 'putus SD', 'wirausaha', 'Lebih dari 3 juta', '0898989889', NULL, NULL, NULL, '2020 / 2021', 78, '2020-08-01 08:28:33', 0, '2020-08-01 15:28:33');
 
 -- --------------------------------------------------------
 
@@ -321,10 +343,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `notelp`, `level`, `is_active`, `created_by`, `date_created`, `modified_by`, `modified_date`) VALUES
-(37, 'admin', '8cb2237d0679ca88db6464eac60da96345513964', '', '', 'admin', '1', NULL, '2020-05-08 14:50:22', NULL, '2020-05-08 14:49:58'),
+(37, 'admin', '8cb2237d0679ca88db6464eac60da96345513964', 'admin@admin.com', '981871312', 'admin', '1', NULL, '2020-05-08 14:50:22', 37, '2020-08-01 06:29:08'),
 (51, 'panitia', '8cb2237d0679ca88db6464eac60da96345513964', '', '', 'guru', '1', 37, '2020-06-21 08:50:04', NULL, '0000-00-00 00:00:00'),
 (56, 'user', '8cb2237d0679ca88db6464eac60da96345513964', 'user@user.com', '08989898', 'user', '1', NULL, '2020-06-28 06:05:27', NULL, '2020-06-28 13:05:27'),
-(66, 'user1', '8cb2237d0679ca88db6464eac60da96345513964', 'email@email.com', '0892398913', 'user', '1', NULL, '2020-06-30 05:07:45', NULL, '2020-06-30 12:07:45');
+(66, 'user1', '8cb2237d0679ca88db6464eac60da96345513964', 'email@email.com', '0892398913', 'user', '1', NULL, '2020-06-30 05:07:45', NULL, '2020-06-30 12:07:45'),
+(67, 'toyib', '8cb2237d0679ca88db6464eac60da96345513964', 'aaa@aaa.cp', '08989898', 'user', '1', NULL, '2020-07-05 12:40:57', NULL, '2020-07-05 19:40:57'),
+(68, 'nikmah', '8cb2237d0679ca88db6464eac60da96345513964', 'nik@nik.com', '91873981739', 'user', '1', NULL, '2020-07-06 10:07:45', NULL, '2020-07-06 17:07:45'),
+(69, 'leader', '8cb2237d0679ca88db6464eac60da96345513964', 'kepalaSekolah@sekolah.com', '08989328829', 'kepala', '1', NULL, '2020-07-24 13:34:18', NULL, '2020-07-24 13:34:18'),
+(70, '09809898', 'e9db3b810e12b9d78f33e16eb512ecdade4aa979', 'email@rmail.com', '089898989898', 'user', '1', NULL, '2020-08-01 13:27:37', NULL, '2020-08-01 13:27:37'),
+(71, 'user12', 'e45ed40f34005e1636649ab18bbd16ada02cb251', 'email@email.com', '879879', 'user', '1', NULL, '2020-08-01 13:31:35', NULL, '2020-08-01 13:31:35'),
+(72, ' kokoko', 'cc5119c39a68cc5feb415116944afbfeb905e315', 'asds@sasd.com', '87876', 'user', '1', NULL, '2020-08-01 13:35:14', NULL, '2020-08-01 13:35:14'),
+(73, 'userss', '6de61f2e4017765d064df7fe10c9e585006ac7e0', 'sdda@sada.com', '987979', 'user', '1', NULL, '2020-08-01 13:37:31', NULL, '2020-08-01 13:37:31'),
+(74, 'panitia1', '8cb2237d0679ca88db6464eac60da96345513964', 'email@email.com', '12345', 'guru', '1', 37, '2020-08-01 06:40:49', NULL, '2020-08-01 13:40:49'),
+(75, 'panitia2', '8cb2237d0679ca88db6464eac60da96345513964', 'panitia2@panitia.com', '0989013', 'guru', '1', 37, '2020-08-01 07:32:39', NULL, '2020-08-01 14:32:39'),
+(76, 'farel', 'b6f9d7747eaf4615828252e596e7209b9c2da589', 'email@email.com', '089898989898', 'user', '1', NULL, '2020-08-01 14:44:14', NULL, '2020-08-01 14:44:14'),
+(77, 'farel', 'b6f9d7747eaf4615828252e596e7209b9c2da589', 'email@email.com', '089898989898', 'user', '1', NULL, '2020-08-01 14:48:59', NULL, '2020-08-01 14:48:59'),
+(78, '123445', '8cb2237d0679ca88db6464eac60da96345513964', 'email@email.com', '08989898', 'user', '1', NULL, '2020-08-01 08:17:10', NULL, '2020-08-01 15:17:10'),
+(79, '12345', '8cb2237d0679ca88db6464eac60da96345513964', 'email@email.com', '08928732213', 'user', '1', NULL, '2020-08-04 17:27:31', NULL, '2020-08-05 00:27:31');
 
 --
 -- Indexes for dumped tables
@@ -391,49 +426,49 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `gelombang`
 --
 ALTER TABLE `gelombang`
-  MODIFY `id_gelombang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_gelombang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `pengumuman`
 --
 ALTER TABLE `pengumuman`
-  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `ppdb`
 --
 ALTER TABLE `ppdb`
-  MODIFY `id_ppdb` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_ppdb` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- Constraints for dumped tables
@@ -455,7 +490,7 @@ ALTER TABLE `nilai`
 -- Constraints for table `ppdb`
 --
 ALTER TABLE `ppdb`
-  ADD CONSTRAINT `ppdb_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `ppdb_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `siswa`
